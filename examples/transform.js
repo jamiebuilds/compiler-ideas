8df0c77a729ba7440cf8587bb721b6e793b29a46
James Kyle <me@thejameskyle.com>
2017-10-18 12:40:15 +1100

    add comments

--- examples/transform.js
+++ examples/transform.js
@@ -4,10 +4,12 @@ import { transform, match, type ASTNode } from '../src/index';
 const transformer = (path, context) => match(path.node.type, {
   Identifier() {
     let name = path.node.name.split('').reverse().join('');
+    // Replace
     return { ...path.node, name };
   },
 
   else() {
+    // Don't touch
     return path;
   },
 });
