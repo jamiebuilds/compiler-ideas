86a64e88520057386b1d0b8d13d77f473e5b2cb0
James Kyle <me@thejameskyle.com>
2017-10-18 16:56:21 +1100

    get rid of queue and stack

--- src/transform.js
+++ src/transform.js
@@ -1,8 +1,6 @@
 // @flow
 import type { ASTNode } from './types';
 import Path from './path';
-import Stack from './stack';
-import Queue from './queue';
 
 export default async function transform<Context: {}>(
   ast: ASTNode,
@@ -12,24 +10,9 @@ export default async function transform<Context: {}>(
   ) => Path | ASTNode | null,
   context: Context,
 ) {
-  let stack: Stack<Queue<Path>> = new Stack();
-  let queue: Queue<Path> = new Queue();
-  let path: Path = new Path(ast, null, null);
-
-  queue.enqueue(path);
-  stack.push(queue);
-
   let transformed = ast;
 
-  while (stack.size() > 0) {
-    let queue: Queue<Path> = stack.pop();
-
-    while (queue.size() > 0) {
-      let path = queue.dequeue();
-
-      // ...
-    }
-  }
+  // ...
 
   return transformed;
 }
--- src/traverse.js
+++ src/traverse.js
@@ -1,30 +1,12 @@
 // @flow
 import type { ASTNode } from './types';
 import Path from './path';
-import Stack from './stack';
-import Queue from './queue';
 
 export default function traverse<Context: {}>(
   ast: ASTNode,
   traverser: (path: Path, context: Context) => mixed,
   context: Context,
 ): Context {
-  let stack: Stack<Queue<Path>> = new Stack();
-  let queue: Queue<Path> = new Queue();
-  let path: Path = new Path(ast, null, null);
-
-  queue.enqueue(path);
-  stack.push(queue);
-
-  while (stack.size() > 0) {
-    let queue = stack.pop();
-
-    while (queue.size() > 0) {
-      let path = queue.dequeue();
-
-      // ...
-    }
-  }
-
+  // ...
   return context;
 }
